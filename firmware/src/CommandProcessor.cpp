ddb205b3cf6edb1d68af09e671c3d1457452483e
Jack Eastman <kedethuong1111@gmail.com>
2023-03-19 14:58:57 +0700

    Remove Speaker module

--- firmware/src/CommandProcessor.cpp
+++ firmware/src/CommandProcessor.cpp
@@ -1,6 +1,5 @@
 #include <Arduino.h>
 #include "CommandProcessor.h"
-#include "Speaker.h"
 
 const char *words[] = {
     "ON",
@@ -28,22 +27,17 @@ void CommandProcessor::processCommand(uint16_t commandIndex)
     {
     case 0: // ON
         digitalWrite(GPIO_NUM_21, LOW);
-        //m_speaker->playReady();
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
         break;
     case 1: // OFF
         digitalWrite(GPIO_NUM_21, HIGH);
-        //m_speaker->playReady();
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
         break;
     }
 
     //digitalWrite(GPIO_NUM_2, LOW);
 }
 
-CommandProcessor::CommandProcessor(Speaker *speaker)
+CommandProcessor::CommandProcessor()
 {
-    m_speaker = speaker;
     pinMode(GPIO_NUM_2, OUTPUT);
     pinMode(GPIO_NUM_21, OUTPUT);
 
@@ -56,10 +50,6 @@ CommandProcessor::CommandProcessor(Speaker *speaker)
     // kick off the command processor task
     TaskHandle_t command_queue_task_handle;
     xTaskCreate(commandQueueProcessorTask, "Command Queue Processor", 1024, this, 1, &command_queue_task_handle);
-    delete m_speaker;
-    m_speaker = NULL;
-    uint32_t free_ram = esp_get_free_heap_size();
-    Serial.printf("Free ram after playing notification %d\n", free_ram);
 }
 
 void CommandProcessor::queueCommand(uint16_t commandIndex, float best_score, uint8_t numCommand)
